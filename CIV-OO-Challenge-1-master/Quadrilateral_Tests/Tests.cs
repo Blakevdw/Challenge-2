0f0ead0eb4d2e89b25310ab9280101ee3bff5c6f
Blake <48078206+Blakevdw@users.noreply.github.com>
2019-10-16 09:35:43 +1100

    Update 3

--- CIV-OO-Challenge-1-master/Quadrilateral_Tests/Tests.cs
+++ CIV-OO-Challenge-1-master/Quadrilateral_Tests/Tests.cs
@@ -2,78 +2,77 @@
 using NUnit.Framework;
 using Shapes;
 
-namespace Quadrilateral_Tests {
-    [TestFixture]
+namespace Quadrilateral_Tests
+{
+	[TestFixture]
 
 
-    public class Tests {
-        Square sq1 { get; set; }
-        Rectangle rec1 { get; set; }
-       
-        Square sq2 { get; set; }
-        Rectangle rec2 { get; set; }
+	public class Tests
+	{
+		Square sq1 { get; set; }
+		Rectangle rec1 { get; set; }
+		Equilateral equ1 { get; set; }
+		RightAngleTriangle rat1 { get; set; }
+		Circle Cir1 { get; set; }
 
-        Square sq3 { get; set; }
-        Rectangle rec3 { get; set; }
-
-        Square sq4 { get; set; }
-        Rectangle rec4 { get; set; }
-
-        Square sq5 { get; set; }
-        Rectangle rec5 { get; set; }
-
-        [SetUp]
-        public void Init() {
-            sq1 = new Square("Blue", 4);
-            rec1 = new Rectangle("Red", 4, 3);
-            
-            sq2 = new Square("Blue", 6);
-            rec2 = new Rectangle("Red", 5, 4);
-
-            sq3 = new Square("Blue", 5);
-            rec3 = new Rectangle("Red", 5, 10);
-
-            sq4 = new Square("Blue", 15);
-            rec4 = new Rectangle("Red", 4, 6);
-
-            sq5 = new Square("Blue", 7);
-            rec5 = new Rectangle("Red", 9, 7);
-        }
-        
-        [Test]
-        public void SquareTests() {
-            int area = 16;
-            int perimeter = 16;
-            
-            Assert.AreEqual(area, sq1.GetArea());
-            Assert.AreEqual(perimeter, sq1.GetPerimeter());
-            
-            area = 36;
-            perimeter = 24;
-            
-            Assert.AreEqual(area, sq2.GetArea());
-            Assert.AreEqual(perimeter, sq2.GetPerimeter());
-
-            area = 25;
-            perimeter = 20;
-
-            Assert.AreEqual(area, sq3.GetArea());
-            Assert.AreEqual(perimeter, sq3.GetPerimeter());
-
-            area = 225;
-            perimeter = 60;
-
-            Assert.AreEqual(area, sq4.GetArea());
-            Assert.AreEqual(perimeter, sq4.GetPerimeter());
+		Square sq2 { get; set; }
+		Rectangle rec2 { get; set; }
+		Equilateral equ2 { get; set; }
+		RightAngleTriangle rat2 { get; set; }
+		Circle Cir2 { get; set; }
 
 
-            area = 49;
-            perimeter = 28;
+		Square sq3 { get; set; }
+		Rectangle rec3 { get; set; }
+		Equilateral equ3 { get; set; }
+		RightAngleTriangle rat3 { get; set; }
+		Circle Cir3 { get; set; }
 
-            Assert.AreEqual(area, sq5.GetArea());
-            Assert.AreEqual(perimeter, sq5.GetPerimeter());
+		[SetUp]
+		public void Init()
+		{
+			sq1 = new Square("Blue", 5);
+			rec1 = new Rectangle("Red", 5, 10);
+			equ1 = new Equilateral("Blue", 5);
+			rat1 = new RightAngleTriangle("Orange", 4, 6);
+			Cir1 = new Circle("Red", 5);
 
-        }
+			sq2 = new Square("Blue", 15);
+			rec2 = new Rectangle("Red", 4, 6);
+			equ2 = new Equilateral("Blue", 15);
+			rat2 = new RightAngleTriangle("Orange", 5, 10);
+			Cir2 = new Circle("Red", 15);
+
+			sq3 = new Square("Blue", 7);
+			rec3 = new Rectangle("Red", 9, 7);
+			equ3 = new Equilateral("Blue", 7);
+			rat3 = new RightAngleTriangle("Orange", 9, 7);
+			Cir3 = new Circle("Red", 7);
+
+		}
+
+		[Test]
+		public void SquareTests()
+		{
+			int area = 25;
+			int perimeter = 20;
+
+			Assert.AreEqual(area, sq1.GetArea());
+			Assert.AreEqual(perimeter, sq1.GetPerimeter());
+
+			area = 225;
+			perimeter = 60;
+
+			Assert.AreEqual(area, sq2.GetArea());
+			Assert.AreEqual(perimeter, sq2.GetPerimeter());
+
+			area = 49;
+			perimeter = 28;
+
+			Assert.AreEqual(area, sq3.GetArea());
+			Assert.AreEqual(perimeter, sq3.GetPerimeter());
+
+		}
 
 		[Test]
 		[TestCase(5, 25)]
@@ -94,38 +93,95 @@ namespace Quadrilateral_Tests {
 
 
 		[Test]
-        public void RectangleTests() {
-            int area = 12;
-            int perimeter = 14;
-            
-            Assert.AreEqual(area, rec1.GetArea());
-            Assert.AreEqual(perimeter, rec1.GetPerimeter());
+		public void RectangleTests()
+		{
+			int area = 50;
+			int perimeter = 30;
 
-            area = 20;
-            perimeter = 18;
-            
-            Assert.AreEqual(area, rec2.GetArea());
-            Assert.AreEqual(perimeter, rec2.GetPerimeter());
+			Assert.AreEqual(area, rec1.GetArea());
+			Assert.AreEqual(perimeter, rec1.GetPerimeter());
 
-            area = 50;
-            perimeter = 30;
+			area = 24;
+			perimeter = 20;
 
-            Assert.AreEqual(area, rec3.GetArea());
-            Assert.AreEqual(perimeter, rec3.GetPerimeter());
+			Assert.AreEqual(area, rec2.GetArea());
+			Assert.AreEqual(perimeter, rec2.GetPerimeter());
 
-            area = 24;
-            perimeter = 20;
+			area = 63;
+			perimeter = 32;
 
-            Assert.AreEqual(area, rec4.GetArea());
-            Assert.AreEqual(perimeter, rec4.GetPerimeter());
+			Assert.AreEqual(area, rec3.GetArea());
+			Assert.AreEqual(perimeter, rec3.GetPerimeter());
 
-            area = 63;
-            perimeter = 32;
+		}
 
-            Assert.AreEqual(area, rec5.GetArea());
-            Assert.AreEqual(perimeter, rec5.GetPerimeter());
+		[Test]
+		public void EquilateralTest()
+		{
+			int area = 10.83;
+			int perimeter = 15;
 
-        }
-        
-    }
+			Assert.AreEqual(area, equ1.GetArea()));
+			Assert.AreEqual(area, equ1.GetPerimeter()));
+
+			int area = 97.43;
+			int perimeter = 45;
+
+			Assert.AreEqual(area, equ2.GetArea()));
+			Assert.AreEqual(area, equ2.GetPerimeter()));
+
+			int area = 21.22;
+			int perimeter = 21;
+
+			Assert.AreEqual(area, equ3.GetArea()));
+			Assert.AreEqual(area, equ3.GetPerimeter()));
+
+		}
+
+		[Test]
+		public void RightAngleTriangleTest()
+		{
+			int area = 25;
+			int perimeter = 26.18;
+
+			Assert.AreEqual(area, rat1.GetArea()));
+			Assert.AreEqual(perimeter, rat1.GetPerimeter()));
+
+			int area = 12;
+			int perimeter = 17.21;
+
+			Assert.AreEqual(area, rat2.GetArea()));
+			Assert.AreEqual(perimeter, rat2.GetPerimeter()));
+
+			int area = 31.5;
+			int perimeter = 27.4;
+
+			Assert.AreEqual(area, rat3.GetArea()));
+			Assert.AreEqual(perimeter, rat3.GetPerimeter()));
+		}
+
+		[Test]
+		public void CircleTest()
+		{
+			int area = 78.55;
+			int perimeter = 31.42;
+
+			Assert.AreEqual(area, Cir1.GetArea()));
+			Assert.AreEqual(perimeter, Cir1.GetPerimeter()));
+
+			int area = 706.95;
+			int perimeter = 94.26;
+
+			Assert.AreEqual(area, Cir2.GetArea()));
+			Assert.AreEqual(perimeter, Cir2.GetPerimeter()));
+
+			int area = 153.96;
+			int perimeter = 43.99;
+
+			Assert.AreEqual(area, Cir3.GetArea()));
+			Assert.AreEqual(perimeter, Cir3.GetPerimeter()));
+		}
+
+
+	}
 }
--- CIV-OO-Challenge-1-master/Shapes/Shape.cs
+++ CIV-OO-Challenge-1-master/Shapes/Shape.cs
@@ -17,44 +17,4 @@ namespace Shapes
 		}
 	}
 
-	public class Quadrilateral : Shape
-	{
-		public int Side1Length { get; set; }
-		public int Side2Length { get; set; }
-		public int Side3Length { get; set; }
-		public int Side4Length { get; set; }
-
-		public Quadrilateral(string pColour, int pSide1Length, int pSide2Length, int pSide3Length, int pSide4Length) : base(pColour)
-		{
-			Side1Length = pSide1Length;
-			Side2Length = pSide2Length;
-			Side3Length = pSide3Length;
-			Side4Length = pSide4Length;
-		}
-
-		public int GetPerimeter()
-		{
-			return Side1Length + Side2Length + Side3Length + Side4Length;
-		}
-	}
-
-	public class Square : Quadrilateral
-	{
-		public Square(string pColour, int pSide1Length) : base(pColour, pSide1Length, pSide1Length, pSide1Length, pSide1Length) { }
-
-		public int GetArea()
-		{
-			return Side1Length * Side1Length;
-		}
-	}
-
-	public class Rectangle : Quadrilateral
-	{
-		public Rectangle(string pColour, int pSide1Length, int pSide2Length) : base(pColour, pSide1Length, pSide2Length, pSide1Length, pSide2Length) { }
-
-		public int GetArea()
-		{
-			return Side1Length * Side2Length;
-		}
-	}
 }
